8e9ed488e5a31ab3136c84ff0d2dce029db3d92c
Your Name <naqedshaker139@gmail.com>
2026-01-13 17:13:14 +0300

    Add login page and route in src/pages/App.jsx

---
+++ equipment-management-frontend/src/pages/App.jsx
@@ -0,0 +1,45 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Layout from '../components/Layout';
+import Dashboard from '../components/Dashboard';
+import EquipmentList from '../components/EquipmentList';
+import DriverList from '../components/DriverList';
+import RequestForm from '../components/RequestForm';
+import RequestHistory from '../components/RequestHistory';
+import TimesheetLauncher from './TimesheetLauncher';
+import TimesheetCard from './TimesheetCard';
+import Login from './Login'; // NEW
+import '../api'; // Configure axios
+import '../i18n';
+import '../App.css';
+
+function App() {
+  const { i18n } = useTranslation();
+  
+  // Set document direction based on language
+  document.dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
+
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          {/* Login page */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Existing routes */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/equipment" element={<EquipmentList />} />
+          <Route path="/drivers" element={<DriverList />} />
+          <Route path="/request" element={<RequestForm />} />
+          <Route path="/requests" element={<RequestHistory />} />
+
+          {/* Timesheet routes */}
+          <Route path="/timesheets/new" element={<TimesheetLauncher />} />
+          <Route path="/timesheets/:id" element={<TimesheetCard />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
